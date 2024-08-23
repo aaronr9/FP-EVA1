4d7df97955aba920749281a56a7719fd4cc8100a
aaronr9 <L24550244@chihuahua2.tecnm.mx>
2024-08-23 16:15:52 -0600

    Add files via upload

---
+++ EVA1_1_HOLA_MUNDO/src/main/java/com/mycompany/eva1_1_hola_mundo/EVA1_1_HOLA_MUNDO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_1_hola_mundo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_1_HOLA_MUNDO {
+
+    public static void main(String[] args) {//MOSTRAR UN SALUDO --> DOCUMENTAR
+        System.out.println("Hello World!");
+    }
+}
