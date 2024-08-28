933a48442b4232adc73e6ae11e65e02653419e1b
aaronr9 <L24550244@chihuahua2.tecnm.mx>
2024-08-28 16:06:52 -0600

    Add files via upload

---
+++ EVA1_3_AUTOMOVIL/src/main/java/com/mycompany/eva1_3_automovil/EVA1_3_AUTOMOVIL.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_3_automovil;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_3_AUTOMOVIL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //DECLARACION DE LAS VARIABLES
+        String marca;
+        String modelo;
+        int year;
+        double precio;
+        
+        //ASIGNAR VALORES
+        marca = "FORD";
+        modelo = "MUSTANG";
+        year = 2022;
+        precio = 999000.1;
+        
+        System.out.println(marca);//imprime el contenido de la variable
+        System.out.println(modelo);
+        System.out.println(year);
+        System.out.println(precio);
+    }
+}
