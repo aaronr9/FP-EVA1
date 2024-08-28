edffe30e5385f7713aa446878859f06d74fba248
aaronr9 <L24550244@chihuahua2.tecnm.mx>
2024-08-28 16:07:33 -0600

    Add files via upload

---
+++ EVA1_4_TELEVISION/src/main/java/com/mycompany/eva1_4_television/EVA1_4_TELEVISION.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_4_television;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_4_TELEVISION {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //DECLARACION DE LAS VARIABLES
+        String marca;
+        String modelo;
+        int pulgadas;
+        int precio;
+        int year;
+        
+        //ASIGNAR VALORES
+        marca = "SAMSUNG";
+        modelo= "Crystal UHD";
+        pulgadas = 80;
+        precio = 50000;
+        year = 2022;
+        
+        System.out.println(marca);   //imprime el contenido de la variable
+        System.out.println(modelo);
+        System.out.println(pulgadas);
+        System.out.println(precio);
+        System.out.println(year);
+        
+        
+    }
+}
