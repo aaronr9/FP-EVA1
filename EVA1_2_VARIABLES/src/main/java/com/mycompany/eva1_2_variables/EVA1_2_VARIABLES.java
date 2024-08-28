86a8d008f60dec1c7740fbb37ca45adb5fbbb451
aaronr9 <L24550244@chihuahua2.tecnm.mx>
2024-08-28 16:03:03 -0600

    Add files via upload

---
+++ EVA1_2_VARIABLES/src/main/java/com/mycompany/eva1_2_variables/EVA1_2_VARIABLES.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_2_variables;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_2_VARIABLES {
+
+    public static void main(String[] args) {
+                                            //TIPOS DE DATOS EN JAVA
+        System.out.println("Hola mundo!!");//cadena de texto
+        System.out.println(100);       //un numero entero
+        System.out.println('c');       //un caracter
+        System.out.println(true);      //valores lógicos
+        System.out.println(false);
+        System.out.println(1000.52);    //números con fracción (con punto flotante)
+        
+        //TIPOS DE DATOS
+        //ENTEROS
+        //int
+        //long
+        //byte
+        //short
+        //PUNTO FLOTANTE
+        //double
+        //float
+        //TEXTO
+        //String
+        //CARACTER
+        //char
+        //LÓGICOS (true false)
+        //boolean
+        
+        //creacion y uso de variables
+        int edad; //Declaro una variable de tipo entero, llama
+                  //edad
+        //double edad; //NO PODEMOS TENER DOS VARIABLES CON EL MISMO NOMBRE
+        String nombre;
+        String apellido;
+        double promedio;
+        
+        edad = 18; //estoy asignando el valor 18 dentro de la 
+                   //variable edad
+                 
+       
+       System.out.println(edad);// println tambien imprime variables
+        
+        
+   }
+}
