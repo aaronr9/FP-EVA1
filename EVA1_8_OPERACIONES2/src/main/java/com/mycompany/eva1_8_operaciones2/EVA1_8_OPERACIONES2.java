57ec9afee885047f4aa908528b1d6a2283ccf637
aaronr9 <L24550244@chihuahua2.tecnm.mx>
2024-09-09 21:14:41 -0600

    Add files via upload

---
+++ EVA1_8_OPERACIONES2/src/main/java/com/mycompany/eva1_8_operaciones2/EVA1_8_OPERACIONES2.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_8_operaciones2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_8_OPERACIONES2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String nombre = "Aaron";
+        String apellido = "Ruiz Ortiz";
+        String nombreCom;
+        //CADENAS DE TEXTO, LA OPERACION SE LLAMA
+        //CONCATENACION --> UNIR CADENAS DE TEXTO
+        //EL OPERADOR DE CONCATENACION ES EL +
+        //CUANDO UN SIMBOLO TIENE VARIAS FUNCIONES
+        //SE LE LLAMA SOBRECARGA (DE OPERACIONES)
+        nombreCom = nombre +" " + apellido;
+        
+        System.out.println("El nombre es: " + nombre);
+        System.out.println("El apellido es: " + apellido);
+        System.out.println("El nombre es :" + nombreCom);
+        
+        
+    }
+}
