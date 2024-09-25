83244f460425ca33bc4706a1114def28115a6a32
aaronr9 <L24550244@chihuahua2.tecnm.mx>
2024-09-25 15:58:26 -0600

    Add files via upload

---
+++ EVA1_15_PANTALLA_INICIO/src/main/java/com/mycompany/eva1_15_pantalla_inicio/EVA1_15_PANTALLA_INICIO.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_15_pantalla_inicio;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_15_PANTALLA_INICIO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Scanner captu = new Scanner(System.in);
+        
+        
+        
+        
+    }
+}
