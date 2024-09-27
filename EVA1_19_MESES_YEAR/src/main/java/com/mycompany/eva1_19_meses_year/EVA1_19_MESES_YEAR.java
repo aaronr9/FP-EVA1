a142b8ed8bbc4d19e38ea98bce91fa4f6f85b43b
aaronr9 <L24550244@chihuahua2.tecnm.mx>
2024-09-26 20:23:46 -0600

    Add files via upload

---
+++ EVA1_19_MESES_YEAR/src/main/java/com/mycompany/eva1_19_meses_year/EVA1_19_MESES_YEAR.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_19_meses_year;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario
+ */
+public class EVA1_19_MESES_YEAR {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int mes;
+        Scanner captu = new Scanner(System.in);
+        System.out.println("Numero correspondiente al dia (1-12):");
+        mes = captu.nextInt();
+        
+        if(mes == 1){
+            System.out.println("Enero");
+        }else if(mes == 2){
+            System.out.println("Febrero");        
+        }else if(mes == 3){
+            System.out.println("Marzo");        
+        }else if(mes == 4){
+            System.out.println("Abril");        
+        }else if(mes == 5){
+            System.out.println("Mayo");        
+        }else if(mes == 6){
+            System.out.println("Junio");        
+        }else if(mes == 7){
+            System.out.println("Julio"); 
+        }else if(mes == 8){
+            System.out.println("Agosto"); 
+        }else if(mes == 9){
+             System.out.println("Septiembre");    
+        }else if(mes == 10){
+            System.out.println("Octubre"); 
+        }else if(mes == 11){
+            System.out.println("Noviembre"); 
+        }else if(mes == 12){
+            System.out.println("Diciembre"); 
+            
+        }else{//Seleccion opcional
+            System.out.println("Numero no valido (deben ser entre 1 y 12)");
+        }
+    }
+}
+    
+
